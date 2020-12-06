68cdca93b4ca8f5ce4fbb64fc7aad1516aee60d2
Holly McLean <hollym509@gmail.com>
2020-12-06 11:26:15 -0800

    Setup initial dice rolling menu design

--- App-For-Friends/Form1.Designer.cs
+++ App-For-Friends/Form1.Designer.cs
@@ -28,13 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.d4button = new System.Windows.Forms.Button();
+            this.d6button = new System.Windows.Forms.Button();
+            this.d8button = new System.Windows.Forms.Button();
+            this.d10button = new System.Windows.Forms.Button();
+            this.d12button = new System.Windows.Forms.Button();
+            this.d20button = new System.Windows.Forms.Button();
+            this.dPercentButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.28823F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.38995F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.69322F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.11217F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.51642F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.d4button);
+            this.flowLayoutPanel1.Controls.Add(this.d6button);
+            this.flowLayoutPanel1.Controls.Add(this.d8button);
+            this.flowLayoutPanel1.Controls.Add(this.d10button);
+            this.flowLayoutPanel1.Controls.Add(this.d12button);
+            this.flowLayoutPanel1.Controls.Add(this.d20button);
+            this.flowLayoutPanel1.Controls.Add(this.dPercentButton);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(116, 58);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(567, 85);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // d4button
+            // 
+            this.d4button.Location = new System.Drawing.Point(3, 3);
+            this.d4button.Name = "d4button";
+            this.d4button.Size = new System.Drawing.Size(75, 23);
+            this.d4button.TabIndex = 0;
+            this.d4button.Text = "d4";
+            this.d4button.UseVisualStyleBackColor = true;
+            // 
+            // d6button
+            // 
+            this.d6button.Location = new System.Drawing.Point(84, 3);
+            this.d6button.Name = "d6button";
+            this.d6button.Size = new System.Drawing.Size(75, 23);
+            this.d6button.TabIndex = 1;
+            this.d6button.Text = "d6";
+            this.d6button.UseVisualStyleBackColor = true;
+            // 
+            // d8button
+            // 
+            this.d8button.Location = new System.Drawing.Point(165, 3);
+            this.d8button.Name = "d8button";
+            this.d8button.Size = new System.Drawing.Size(75, 23);
+            this.d8button.TabIndex = 2;
+            this.d8button.Text = "d8";
+            this.d8button.UseVisualStyleBackColor = true;
+            // 
+            // d10button
+            // 
+            this.d10button.Location = new System.Drawing.Point(246, 3);
+            this.d10button.Name = "d10button";
+            this.d10button.Size = new System.Drawing.Size(75, 23);
+            this.d10button.TabIndex = 3;
+            this.d10button.Text = "d10";
+            this.d10button.UseVisualStyleBackColor = true;
+            // 
+            // d12button
+            // 
+            this.d12button.Location = new System.Drawing.Point(327, 3);
+            this.d12button.Name = "d12button";
+            this.d12button.Size = new System.Drawing.Size(75, 23);
+            this.d12button.TabIndex = 4;
+            this.d12button.Text = "d12";
+            this.d12button.UseVisualStyleBackColor = true;
+            // 
+            // d20button
+            // 
+            this.d20button.Location = new System.Drawing.Point(408, 3);
+            this.d20button.Name = "d20button";
+            this.d20button.Size = new System.Drawing.Size(75, 23);
+            this.d20button.TabIndex = 5;
+            this.d20button.Text = "d20";
+            this.d20button.UseVisualStyleBackColor = true;
+            // 
+            // dPercentButton
+            // 
+            this.dPercentButton.Location = new System.Drawing.Point(489, 3);
+            this.dPercentButton.Name = "dPercentButton";
+            this.dPercentButton.Size = new System.Drawing.Size(75, 23);
+            this.dPercentButton.TabIndex = 6;
+            this.dPercentButton.Text = "d%";
+            this.dPercentButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(300, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll some die!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "An App for D&D Friends";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button d4button;
+        private System.Windows.Forms.Button d6button;
+        private System.Windows.Forms.Button d8button;
+        private System.Windows.Forms.Button d10button;
+        private System.Windows.Forms.Button d12button;
+        private System.Windows.Forms.Button d20button;
+        private System.Windows.Forms.Button dPercentButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
